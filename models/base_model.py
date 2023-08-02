8c3927d25101fcc4ab9c4d886e596d6a6b277aaf
mau4512 <mauricio.25.17@gmail.com>
2023-08-02 13:43:12 -0700

    pushing models

--- models/base_model.py
+++ models/base_model.py
@@ -1,3 +1,40 @@
 #!/usr/bin/python3
 
-print("hello world)
+from datetime import datetime
+from uuid import uuid4
+import models
+
+class BaseModel:
+    """BaseModel for AirBnB project"""
+    def __init__(self):
+        """Initialize a new BaseModel.
+
+        Args:
+            *args (any): Unused.
+            **kwargs (dict): Key/value pairs of attributes.
+        """
+        tform = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def save(self):
+        """Update updated_at with the current datetime."""
+        self.updated_at = datetime.now()
+        models.storage.save()
+
+    def to_dict(self):
+        """Return the dictionary of the BaseModel instance.
+
+        Includes the key/value pair __class__ representing
+        the class name of the object.
+        """
+        my_dict = self.__dict__.copy()
+        my_dict["created_at"] = self.created_at.isoformat()
+        my_dict["updated_at"] = self.updated_at.isoformat()
+        my_dict["__class__"] = self.__class__.__name__
+        return my_dict
+
+    def __str__(self):
+        """Return the print/str representation of the BaseModel instance."""
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
