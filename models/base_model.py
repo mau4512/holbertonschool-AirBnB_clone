849dfb1c53e19f8e0642e7306b763302cb37391a
mau4512 <mauricio.25.17@gmail.com>
2023-08-03 22:03:54 -0700

    pushing new basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -29,7 +29,6 @@ class BaseModel:
             self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            models.storage.new(self)
 
     def save(self):
         """Update updated_at with the current datetime."""
